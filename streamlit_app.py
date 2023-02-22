9e9bfa032eaf508e9cb516be2617dcafbe82b549
anandhusekhar <30618493+anandhusekhar@users.noreply.github.com>
2023-02-22 07:52:04 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 #created new python file
 import streamlit
 streamlit.title('hello')
-streamlit.header('Hi, Noobie')
-streamlit.text('Welcome to the world of programming')
+streamlit.header('Hi, Noobie 🐔')
+streamlit.text('🥣Welcome to the world of programming')
+
+
