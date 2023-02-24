d7384bb02aec1be32795f8d2479cc7007f8ea7af
anandhusekhar <30618493+anandhusekhar@users.noreply.github.com>
2023-02-24 14:24:42 +0530

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 #created new python file
 import streamlit
+import pandas as pd
 streamlit.title('hello')
 streamlit.header('Hi, Noobie 🐔')
 streamlit.text('🥣Welcome to the world of programming')
+df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
 
 
