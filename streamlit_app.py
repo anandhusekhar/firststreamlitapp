e375af747e62120dd4d55fd742baa5c56e703a2a
anandhusekhar <anandhusekhar52gmail.com>
2023-03-03 07:39:52 +0530

    Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('🥣Welcome to the world of programming')
 df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 list1=df.set_index('Serving_Size')
 # streamlit.multiselect("pick some fruits:", list(list1.index),['Avocado','Strawberries'])
-streamlit.dataframe(list1)
+
 
 
 
