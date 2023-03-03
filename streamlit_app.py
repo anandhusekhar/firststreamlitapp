9dc94d5db933f33b19581a887774f653e33e173a
anandhusekhar <anandhusekhar5@gmail.com>
2023-03-03 08:59:19 +0530

    adsad

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,10 @@ streamlit.title('hello')
 streamlit.header('Hi, Noobie 🐔')
 streamlit.text('🥣Welcome to the world of programming')
 myfruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# list1=df.set_index('Fruit')
-streamlit.multiselect("pick some fruits:", list(myfruit_list.index))
+myfruit_list=df.set_index('Fruit')
+fruits_selected=streamlit.multiselect("pick some fruits:", list(myfruit_list.index))
 # fruits_to_show=df.loc[fruits_selected]
-# streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_selected)
 # streamlit.dataframe(fruits_selected)
 
 
