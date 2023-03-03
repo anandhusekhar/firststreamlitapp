2f0ee9497ead030a1e0e32f4b8e4304e2410e6fc
anandhusekhar <anandhusekhar5@gmail.com>
2023-03-03 08:57:23 +0530

    sas

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import pandas as pd
 streamlit.title('hello')
 streamlit.header('Hi, Noobie 🐔')
 streamlit.text('🥣Welcome to the world of programming')
-df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-list1=df.set_index('Fruit')
-fruits_selected=streamlit.multiselect("pick some fruits:", list(list1.index))
+myfruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# list1=df.set_index('Fruit')
+fruits_selected=streamlit.multiselect("pick some fruits:", list(myfruit_list.index))
 # fruits_to_show=df.loc[fruits_selected]
 # streamlit.dataframe(fruits_selected)
 streamlit.dataframe(fruits_selected)
